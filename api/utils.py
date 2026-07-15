0e6b716bc783227659a5a87b33cdc6e29635eb70
Àngel Ruiz Fas <angruifas.13@gmail.com>
2026-07-15 09:40:25 +0200

    test

--- api/utils.py
+++ api/utils.py
@@ -46,10 +46,8 @@ async def forward_request(path: str, request: Request, x_target_ip: str, files=N
             except httpx.ConnectError:
                 raise HTTPException(status_code=503, detail="Can't connect to fog")
             except:
-                raise HTTPException(status_code=500, detail="Unexpected error")
-            
-        request.app.state.logger.info(f"http://{x_target_ip}/{path}")
-        request.app.state.logger.info(resp.status_code)
+                request.app.state.logger.info(resp.status_code)
+                raise HTTPException(status_code=500, detail="Unexpected error")        
 
     return Response(content=resp.content, status_code=resp.status_code)
 
